23baf0681eb9739d426f132be0bda5ace957acd6
umg <umg@HUEC5.labhue.local>
2023-03-30 20:44:53 -0600

    avances

---
+++ src/main.java
@@ -0,0 +1,78 @@
+
+import java.util.*;
+//import java.util.List;//
+
+
+public class main {
+
+    public static void main(String[] args) {
+       /* List MiLista = new ArrayList();
+        MiLista.add(10);
+        MiLista.add(10.50);
+        MiLista.add("Hola Mundo");
+        MiLista.add(true);
+        System.out.println(MiLista);
+        System.out.println("----Otra forma de correrlo");
+        MiLista.forEach(dato->{
+            System.out.println(dato);
+        });
+        System.out.println("-------Otra forma de correrlo");
+        
+        for(Object ListaDatos: MiLista)
+        {
+            System.out.println(ListaDatos);  
+        }*/
+       
+       
+      /*HashSet ListaNormal = new HashSet();
+       ListaNormal.add("Lunes");
+       ListaNormal.add("Martes");
+       ListaNormal.add("Miercoles");
+       ListaNormal.add("Jueves");
+       ListaNormal.add("Viernes");
+        ListaNormal.add("Viernes");
+       
+       System.out.println(ListaNormal);
+       
+        System.out.println("--------------usando lamda");
+        
+        
+      ListaNormal.forEach(dato->{
+          System.out.println(dato);
+      });
+      
+      ListaNormal.clear();
+      
+      System.out.println("--------------imprimir for abreviado");
+        for(Object MisDatos: ListaNormal)
+        {
+            System.out.println(MisDatos);
+        }
+        
+       */
+      
+      
+      
+      
+   
+  HashMap MiLista = new HashMap();
+  MiLista.put(502, "Guatemala");
+  MiLista.put(503, "El Salvador");
+  MiLista.put(504, "Honduras");
+  MiLista.put(505, "Nicaragua");
+  MiLista.put(506, "Costa Rica");
+  MiLista.put(507, "Panáma"); 
+  
+  System.out.println(MiLista);
+  MiLista.put(507,"Estados Unidos");
+  MiLista.remove(507);
+  System.out.println(MiLista);
+  
+  
+MiLista.forEach((clave,valor)->{
+    System.out.println(clave+""+valor);
+});
+       
+    }
+    
+}
